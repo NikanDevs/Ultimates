5a1b0a79411d4a7766fa404a580f9348b0295ee1
NikanWasTaken <nikan1252sabet4801@gmail.com>
2022-05-15 15:45:22 +0430

    chore(banOptions): added 1-7 days

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -23,22 +23,18 @@ export default new Command({
 		},
 		{
 			name: 'delete_messages',
-			description: 'Deletes the messages sent for the member in a specific duration.',
+			description: 'The amount of days to delete messages for.',
 			type: ApplicationCommandOptionType.Number,
 			required: false,
 			choices: [
-				{
-					name: "Don't delete any",
-					value: 0,
-				},
-				{
-					name: 'Previous 24 hours',
-					value: 1,
-				},
-				{
-					name: 'Previous 7 days',
-					value: 7,
-				},
+				{ name: "Don't delete any", value: 0 },
+				{ name: 'Previous 24 hours', value: 1 },
+				{ name: 'Previous 48 hours', value: 2 },
+				{ name: 'Previous 3 days', value: 3 },
+				{ name: 'Previous 4 days', value: 4 },
+				{ name: 'Previous 5 days', value: 5 },
+				{ name: 'Previous 6 days', value: 6 },
+				{ name: 'Previous 7 days', value: 7 },
 			],
 		},
 		{
