d28c79b116875da77a21cba02e0ae62918d09952
NikanWasTaken <nikanwastaken@users.noreply.github.com>
2022-08-12 14:54:17 +0430

    fix(threadJoin): thread joinable return (#18)

--- src/events/threadCreate.ts
+++ src/events/threadCreate.ts
@@ -2,7 +2,7 @@ import { Event } from '../structures/Event';
 
 export default new Event('threadCreate', async (thread) => {
 	if (thread.joined) return;
-	if (thread.joinable) return;
+	if (!thread.joinable) return;
 
 	await thread.join();
 });
